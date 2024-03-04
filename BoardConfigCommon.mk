032d570c4ef83eecf4585663ae27f05327f1a4fe
Notganesh <chimeports@gmail.com>
2024-03-04 17:47:16 +0000

    mt6877-common:Initial skeleton
    Signed-off-by: Notganesh <chimeports@gmail.com>

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/oneplus/mt6877-common
+
+# Inherit the proprietary files
+include vendor/oneplus/mt6877-common/BoardConfigVendor.mk
---
+++ README.md
@@ -0,0 +1,1 @@
+# Common device tree for Oneplus MT6877 devices
