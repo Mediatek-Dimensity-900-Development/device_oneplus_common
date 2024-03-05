eba8af7ebee13e08d1959c01ee47aec39685663b
bengris32 <bengris32@protonmail.ch>
2024-03-05 11:39:25 +0530

    mt6877-common:Build common keymaster missing libs

--- mt6877.mk
+++ mt6877.mk
@@ -84,6 +84,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor
 
+PRODUCT_PACKAGES += \
+    libkeymaster4.vendor \
+    libkeymaster4support.vendor \
+    libpuresoftkeymasterdevice.vendor \
+    libsoft_attestation_cert.vendor
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.mt6877
@@ -140,6 +146,11 @@ PRODUCT_PACKAGES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.mediatek
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    libkeystore-wifi-hidl \
+    libkeystore-engine-wifi-hidl
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/oneplus/mt6877-common/mt6877-common-vendor.mk)
