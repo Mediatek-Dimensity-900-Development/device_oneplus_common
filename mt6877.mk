13f4169927783e4cbe98ce2f22e133e7295a0402
SebaUbuntu <seba@sebaubuntu.dev>
2024-03-07 11:58:51 +0530

    mt6877-common: Define DT2W node for power HAL

--- mt6877.mk
+++ mt6877.mk
@@ -60,6 +60,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5.vendor \
     android.hardware.camera.provider@2.6.vendor \
 
+PRODUCT_PACKAGES += \
+    libdng_sdk.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service
