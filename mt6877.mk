c20b2ad329d577b1c57c13861abd517ad2a9e6e6
Notganesh <chimeports@gmail.com>
2024-03-08 14:43:57 +0530

    mt6877-common:Upversion drm to 1.4
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- mt6877.mk
+++ mt6877.mk
@@ -10,8 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -96,7 +96,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
     libmockdrmcryptoplugin
